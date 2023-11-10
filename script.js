7f560c4d52093c3187c8fbaa3c4948f181fb43be
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-11-09 23:05:18 -0300

    add calculo de idade e mensagem de erro

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-import { differenceInCalendarDays, differenceInCalendarYears, differenceInCalendarMonths } from 'date-fns';
 
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () =>{
-
-    const day = parseInt(document.querySelector('#day').value);
-    const month = parseInt(document.querySelector('#month').value);
-    const year = parseInt(document.querySelector('#year').value);
+    const day = document.querySelector('#day').value;
+    const month = document.querySelector('#month').value;
+    const year = document.querySelector('#year').value;
     const resultDay = document.querySelector('.day');
     const resultMonth = document.querySelector('.month');
     const resultYear = document.querySelector('.year');
@@ -14,49 +12,125 @@ btn.addEventListener('click', () =>{
     const correntDay = new Date().getDate();
     const correntMonth = new Date().getMonth() + 1;
     const correntYear = new Date().getFullYear();
-    
-    const nascimento = new Date(correntYear, correntMonth +1, correntDay);
-    const atual = new Date(correntDay, correntMonth +1, correntYear)
 
-    
-    const ageDay = differenceInCalendarDays(atual, nascimento);
-    const ageYear = differenceInCalendarYears(atual, nascimento);
-    const ageMonth = differenceInCalendarMonths(atual, nascimento) % 12;
-    
-    resultDay.innerHTML = ageDay;
-    resultMonth.innerHTML = ageMonth;
-    resultYear.innerHTML = ageYear;
-    
+   
     //mensagem de erro
-    //const erro = document.getElementById('error-day');
+    let isValid = true
+
+    const erroRequired = document.querySelectorAll('#erro-required')
+    const erroInput = document.querySelectorAll('.errorr')
+    const erroLabel = document.querySelectorAll('.erro-label')
+
+
+    erroRequired.forEach(erro =>{
+        if (day === "" && month === "" && year === ""){
+            erro.style.display = "block";
+
+            erroInput.forEach(input => {
+                input.style.border = "1px solid hsl(0, 100%, 67%)"
+            })
+
+            erroLabel.forEach(label => {
+                label.style.color = 'hsl(0, 100%, 67%)';
+            })
+        }else{
+            erro.style.display = "none";
+            erroInput.forEach(input => {
+                input.style.border = '1px solid hsl(0, 0%, 86%)';
+            })
+
+            erroLabel.forEach(label => {
+                label.style.color = 'hsl(0, 1%, 44%)';
+            })
+        }
+    })
     
-    /* if (day > 31 || day < 0){
-        erro.style.display = 'block';
+    const erroD = document.getElementById('error-day');
+        
+    if (day > 31 || day < 0){
+        erroD.style.display = 'block';
+        isValid = false;
+
+        erroInput.forEach(input => {
+            input.style.border = "1px solid hsl(0, 100%, 67%)"
+        })
+
+        erroLabel.forEach(label => {
+            label.style.color = 'hsl(0, 100%, 67%)';
+        })
     }
     else{
-         erro.style.display = 'none';
-    } */
-       
+         erroD.style.display = 'none';
+    } 
+
+    const erroM = document.getElementById('error-month');
     
-    //const erro = document.getElementById('error-month');
-    /* 
-    if (month> 12 || month <= 0){
-        erro.style.display = 'block';
+    if (month> 12 || month < 0){
+        erroM.style.display = 'block';
+        isValid = false;
+
+        erroInput.forEach(input => {
+            input.style.border = "1px solid hsl(0, 100%, 67%)"
+        })
+
+        erroLabel.forEach(label => {
+            label.style.color = 'hsl(0, 100%, 67%)';
+        })
     }
     else{
-        erro.style.display = 'none';
-    } */
+        erroM.style.display = 'none';
+    }
     
+    const erroY = document.getElementById('error-year');
     
-    //const erro = document.getElementById('error-year');
-    
-    /* if (year > correntYear){
-        erro.style.display = 'block';
+    if (year > correntYear){
+        erroY.style.display = 'block';
+        isValid = false;
+
+        erroInput.forEach(input => {
+            input.style.border = "1px solid hsl(0, 100%, 67%)"
+        })
+
+        erroLabel.forEach(label => {
+            label.style.color = 'hsl(0, 100%, 67%)';
+        })
     }
     else{
-        erro.style.display = 'none';
-    } */
-     
+        erroY.style.display = 'none';
+        
+
+
+
+    //calculando idade
+
+    if(day !== "" && month !== "" && year !== "" && isValid === true){
+
+        const today = new Date(correntYear, correntMonth, correntDay);
+        
+            
+        let ageDay = correntDay - Number(day);
+        let ageYear = correntYear - Number(year);
+        let ageMonth = correntMonth - Number(month);
+        
+        if (ageDay < 0) {
+            ageMonth -= 1;
+            ageDay += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        }
+                
+        if (ageMonth < 0) {
+            ageYear -= 1;
+            ageMonth += 12;
+        }
+        
+        resultDay.innerHTML = ageDay;
+        resultMonth.innerHTML = ageMonth;
+        resultYear.innerHTML = ageYear;
+        
+        } 
+        
+        
+    }
+    
     
 })
 
